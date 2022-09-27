1eb31826cd727f6873b41392bc78d51360803db9
achrafKhe <khemiri.achraf29@gmail.com>
2022-09-27 18:00:02 +0200

    2nd commit

---
+++ 1stcode.R
@@ -0,0 +1,12 @@
+TRUE == FALSE
+# Comparison of numerics
+-6 * 14 != 17 - 101
+# Comparison of character strings
+"useR" == "user"
+# Compare a logical with a numeric
+TRUE == 1
+
+git config --global user.email "khemiri.achraf29@gmail.com"
+git config --global user.name "achrafKhe"
+
+print("xkvnklsd")
